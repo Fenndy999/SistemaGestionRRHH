1c3875f4eba1ee93cc6147f8e2a470dc0b375fb3
Fenndy999 <fenndycornelio@gmail.com>
2024-10-24 23:30:58 +0200

    Preparando para subir a GitHub

---
+++ proyecto.Domain/Empleado.cs
@@ -0,0 +1,11 @@
+﻿namespace proyecto.Domain
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cargo { get; set; }
+        // Otros atributos
+    }
+}
---
+++ proyecto.Infrastructure/DATA/RRHHContext.cs.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proyecto.Domain; // Asegúrate de importar el espacio de nombres correcto
+
+public class RRHHContext : DbContext
+{
+    public RRHHContext(DbContextOptions<RRHHContext> options) : base(options) { }
+
+    public DbSet<Empleado> Empleados { get; set; }
+
+    // Otros DbSet para otras entidades que agregues en el futuro
+}
